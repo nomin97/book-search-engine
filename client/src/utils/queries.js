cd148b31eaef28cc45b7ca3d2dfd9f240c8a5092
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-12-09 12:31:17 -0700

    mutations in utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
