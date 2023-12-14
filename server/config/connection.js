e58680411714ddf437af441dd81a3725b6267adc
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-12-14 02:56:43 -0700

    trying to fix connection error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://nominm97:facebook1!@localhost/?authMechanism=DEFAULT');
 
 module.exports = mongoose.connection;
